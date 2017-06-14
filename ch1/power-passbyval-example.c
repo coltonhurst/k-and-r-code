93e957d012997b8f07ad718af225cdb2ccbd1215
Colton Hurst <coltvb@gmail.com>
2017-06-14 11:39:30 -0400

    Modified the power files and added the call by val ex

--- ch1/power-passbyval-example.c
+++ ch1/power-passbyval-example.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-int power();
-
 /* test power function */
 main()
 {
@@ -12,15 +10,12 @@ main()
     return 0;
 }
 
-/* power: raise base to n-th power; n>= 0 */
-/*        (old-style version) */
-power(base, n)
-int base, n;
+/* power: raise base to tn-th power; n>=0; version 2 */
+int power(int base, int n)
 {
-    int i, p;
+    int p;
 
-    p = 1;
-    for (i = 1; i <= n; ++i)
+    for (p = 1; n > 0; --n)
         p = p * base;
     return p;
 }
