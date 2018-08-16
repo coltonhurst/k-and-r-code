63e1a5202065a2a6bf28707287df5fb0953087b6
Colton Hurst <colton@coltonhurst.com>
2018-08-16 13:57:43 -0400

    Add vertical histogram

--- book-exercises/ch1/1-13.c
+++ book-exercises/ch1/1-13.c
@@ -1,31 +1,44 @@
 /* K&R page 24 */
 
 /*
-    This prints a histogram of the lengths of words in the input.
+    This prints two histograms based on the lengths of words in the input.
     Compile this file, run it, enter some input, and then send the EOF to see
-    the histogram. It supports words up to a length of 10 characters.
-    The y-axis represents the characters in a word. The x-axis represents
-    how many words. One word is represented by an X.
+    the histograms. It supports words up to a length of 10 characters.
+
+    For the Horizontal Histogram
+    The y-axis represents the length of a word. The x-axis represents
+    how many words are that length. One word is represented by an X.
 
     Example:
+
+    Input: a hi by wi joe bro zoe fam lan
 
     3: XXXXX
     2: XXX
     1: X
 
     The above represents there was 1 word with 1 character, 3 words with 2 characters, and 5 words
-    with 3 characters.
+    with 3 characters in the input.
+
+    For the Vertical histogram, the axis are flipped. It should look like this for the same input:
+    
+        X
+        X
+      X X
+      X X
+    X X X
+    1 2 3 4 5 6 7 8 9 10
 */
 
 #include <stdio.h>
 
-#define IN 		1 /* inside a word */
-#define OUT 	0 /* outside a word */
+#define IN      1 /* inside a word */
+#define OUT     0 /* outside a word */
 
 main()
 {
-    int c, i;
-    c = i = 0;
+    int c, i, j;
+    c = i = j = 0;
     int wordLengths[10];
     int state = OUT;
     int currWordCount = 0;
@@ -37,27 +50,60 @@ main()
 
     /* loop through the input */
     while ((c = getchar()) != EOF) {
+        /* if it's the end of a word */
         if (c == ' ' || c == '\n' || c == '\t') {
             if (state == IN) {
                 state = OUT;
-                wordLengths[currWordCount - 1]++;
+                if ((currWordCount > 0) && (currWordCount < 11))
+                    wordLengths[currWordCount - 1]++;
                 currWordCount = 0;
             }
-        } else {
+        } else { /* if it's at/in a word */
             if (state == OUT)
                 state = IN;
             currWordCount++;
         }
     }
 
+    printf("\n\n");
     /* print the horizontal histogram */
     for (i = 9; i >= 0; i--) {
         printf("%d: ", i + 1);
-        int j;
         for (j = 0; j < wordLengths[i]; j++) {
             printf("X");
         }
         printf("\n");
+    }
+    printf("\n\n");
+
+    /* get the mode of words */
+    int printAnX = 0;
+    int mode = wordLengths[0];
+    for (i = 0; i < 10; i++) {
+        if (wordLengths[i] > mode)
+            mode = wordLengths[i];
+    }
+    /*
+        The mode will be the highest point on
+        our vertical histogram. A mode is
+        the number that occurs more often than
+        any other in a set of numbers. For us, this is
+        highest number of of words that are the same length.
+    */
+    /* print the vertical histogram */
+    for (i = mode; i >= -1; i--) {
+        for (j = 0; j < 10; j++) {
+            if (i > 0) {
+                if (wordLengths[j] >= i)
+                    printf("X ");
+                else
+                    printf("  ");
+            } else if (i == -1) {
+                printf("%d ", j + 1);
+            }
+        }
+        if (i != 0)
+            printf("\n");
     }
 
     return 0;
@@ -66,4 +112,5 @@ main()
 /*
 Notes:
 	- on a mac, ctrl-d to send EOF and see result
+    - This program is well documented on purpose. I hope it helps :)
 */
